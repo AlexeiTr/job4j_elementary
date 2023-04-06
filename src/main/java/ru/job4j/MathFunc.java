479982d0949209583a67aa5af8109dbf7cd0c0fd
Rasp <mcarisma2000@yandex.ru>
2023-04-06 13:47:30 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/MathFunc.java
+++ src/main/java/ru/job4j/MathFunc.java
@@ -1,7 +1,5 @@
 package ru.job4j;
-/** Допишите в методе main код, который вызовет метод func1 с аргументом x = 100,
- *  Результат вычисления запишите в переменную result3.
- */
+
 public class MathFunc {
     public static int func1() {
         int x = 100;
