aa22609b983396326a9716d58636a1f4e6f49fb1
Rasp <mcarisma2000@yandex.ru>
2023-04-06 13:50:22 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/ArgMethod.java
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -1,16 +1,5 @@
 package ru.job4j;
 
-/** Ниже приведен код. Он не компилируется. Программа должна 4 раза вывести на консоль "Hello, Job4j, age = 6".
-
- Ваша задача поправить код, чтобы он компилировался.
-
- Метод hello принимает два аргумента: первый аргумент имеет тип String, второй аргумент имеет тип int.
-
- В методе main мы вызывает 4 раза метод hello. Вам нужно написать правильные аргументы.
-
-
- *
- */
 public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age + " + age);
