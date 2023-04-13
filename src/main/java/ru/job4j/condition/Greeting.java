fa2b0325cae9b174d59564e0576cff9a9a0859d6
Rasp <mcarisma2000@yandex.ru>
2023-04-13 16:19:27 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -4,9 +4,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        idea = idea + "But I am a newbie.";
+        idea += "But I am a newbie.";
         int year = 2023;
-        idea = idea + year;
+        idea += year;
         System.out.println(idea);
 
     }
