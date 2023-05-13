cc75640f3fe1367befd91c00c99598e6288a4e2e
Rasp <mcarisma2000@yandex.ru>
2023-05-13 14:28:41 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,18 +1,14 @@
 package ru.job4j.calculator;
 
-
 public class Fit {
-
     public static double manWeight(short height) {
         double rsl = (height - 100) * 1.15;
         return rsl;
     }
-
     public static double womanWeight(short height) {
         double rsl = (height - 110) * 1.15;
         return rsl;
     }
-
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
