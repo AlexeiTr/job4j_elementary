17f422ef457c564cff0b49002f7a015be1ffddf0
Rasp <mcarisma2000@yandex.ru>
2023-05-28 22:25:37 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -4,7 +4,7 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
+        retur sum(first, second)
                 + multiply(first, second);
     }
 
