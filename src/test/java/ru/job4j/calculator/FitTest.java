2fade392d308cec9fd2a350e335769d85e5648d0
Rasp <mcarisma2000@yandex.ru>
2023-05-29 22:47:36 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -9,7 +9,7 @@ public class FitTest {
 
     @Test
     public void whenMan180Then92() {
-        short in = 18;
+        short in = 180;
         double expected = 92;
         double out = Fit.manWeight(in);
         Assert.assertEquals(expected, out, 0.01);
