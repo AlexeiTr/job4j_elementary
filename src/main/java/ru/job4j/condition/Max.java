6be5c263ecada9b35e78639fad320d672ffc7db3
Rasp <mcarisma2000@yandex.ru>
2023-06-01 14:53:59 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int rsl = Max.max(1,2);
+        System.out.println(rsl);
+    }
+}
