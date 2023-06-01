b8418f6416e6eb06e7134d7db712ce6a5db49b54
Rasp <mcarisma2000@yandex.ru>
2023-06-01 15:12:46 +0300

    4.3.1. Сокращенный оператор && [#333783]

---
+++ src/main/java/ru/job4j/condition/Games.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Games {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the computer club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+
+    }
+}
