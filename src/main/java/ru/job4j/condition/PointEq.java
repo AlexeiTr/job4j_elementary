78ba212d4c773a1b0eff08c2a7bb3ea527025467
Rasp <mcarisma2000@yandex.ru>
2023-06-06 14:27:08 +0300

    4.3.4. if c return [#333785]

--- src/main/java/ru/job4j/condition/PointEq.java
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -2,9 +2,8 @@ package ru.job4j.condition;
 
 public class PointEq {
     public static boolean eq(int x1, int y1, int x2, int y2) {
-        boolean eqX = x1 == x2;
-        boolean eqY = y1 == y2;
-        return eqX && eqY;
+
+        return x1 == x2 && y1 == y2;
     }
 
     public static void main(String[] args) {
