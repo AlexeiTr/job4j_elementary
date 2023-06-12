258dffd04e46570f5eb04d908893371da9d368c2
Rasp <mcarisma2000@yandex.ru>
2023-06-12 12:44:46 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name;
+        switch (day) {
+            case 1:
+                name = " Mondey ";
+                break;
+            case 2:
+                name = " Tuesday ";
+                break;
+            case 3:
+                name = " Wednesday ";
+                break;
+            case 4:
+                name = " Thursday ";
+                break;
+            case 5:
+                name = " Friday ";
+                break;
+            case 6:
+                name = " Saturday ";
+                break;
+            case 7:
+                name = " Sunday ";
+                break;
+            default:
+                name = "Ошибка";
+                break;
+        }
+        return name;
+    }
+}
+
+
