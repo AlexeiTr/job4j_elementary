38a1b8de4e56624e78a33bc04b83432879cc6de0
Rasp <mcarisma2000@yandex.ru>
2023-06-12 12:11:19 +0300

    8. Стороны прямоугольника. [#137735 #415801]

--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -11,7 +11,7 @@ public class SqAreaTest {
     public void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
-        double k = 2;
+        int k = 2;
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
