536c015842584ebb3a3273a872e44ecbcf51445d
Rasp <mcarisma2000@yandex.ru>
2023-07-11 00:04:49 +0300

    5.6. Простые числа [#156314]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
         int count = 0;
         for (int number = 2; number <= finish; number++) {
             if (CheckPrimeNumber.check(number)) {
-           count++;
+                count++;
             }
         }
         return count;
--- src/test/java/ru/job4j/loop/PrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PrimeNumberTest {
