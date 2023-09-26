6dca0ae074f0e04f97765348459e91644c6c367c
Rasp <mcarisma2000@yandex.ru>
2023-09-26 18:24:55 +0300

    6.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/loop/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+          System.out.println(i);
+        }
+    }
+}
