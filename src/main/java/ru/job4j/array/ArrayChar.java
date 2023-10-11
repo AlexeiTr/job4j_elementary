b25f918fd96827d2be14ce31bd19b93f2dfbc66e
Rasp <mcarisma2000@yandex.ru>
2023-10-11 19:05:34 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != word[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
