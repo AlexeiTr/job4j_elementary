9afa55d62691b35558894c7e2c2f45634b3dcff6
Rasp <mcarisma2000@yandex.ru>
2023-10-13 18:27:43 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/loop/DoWhileExample.java
+++ src/main/java/ru/job4j/loop/DoWhileExample.java
@@ -3,14 +3,14 @@ package ru.job4j.loop;
 import java.util.Scanner;
 
 public class DoWhileExample {
-    // Цикл Do-While
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int password;
-        do {                                         // Потом проверка условия! В данном случае ввод пользователя
+        do {
             System.out.println("Введите пароль: ");
             password = scanner.nextInt();
-        } while (password != 555);                   // C начала выполняется код
+        } while (password != 555);
         System.out.println("Пароль введён успешно!");
     }
 }
