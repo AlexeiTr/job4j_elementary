13bcd26e29e525d95a9e3a09154b336910b07cd4
Rasp <mcarisma2000@yandex.ru>
2023-10-17 18:28:32 +0300

    6.6.0. Двумерный массив [#226796]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -4,7 +4,14 @@ public class Sequence {
     public static void main(String[] args) {
         int[][] array = new int[3][3];
         array[0][0] = 1;
-        array[1][1] = 2;
-        array[2][2] = 3;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
+
     }
 }
