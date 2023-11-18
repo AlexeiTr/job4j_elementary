ecf2622f16645b38a2f77344e3188cc8375cc620
Rasp <mcarisma2000@yandex.ru>
2023-11-18 13:51:08 +0300

    6.7.3. Массив из диагонали матрицы.  [#214128]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -22,4 +22,12 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < rsl.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
 }
